baf9aa568c36fd282b581305d3892d9ab174f769
VitaliiKl1m <140404276+VitaliiKl1m@users.noreply.github.com>
2024-03-18 20:03:29 +0200

    Update FindAndReplaceManager.cs

--- Task_2/FindAndReplaceManager.cs
+++ Task_2/FindAndReplaceManager.cs
@@ -5,7 +5,9 @@
 
         public static void FindNext(string str)
         {
-            Console.WriteLine("Searching a string : " + str);
+            Console.WriteLine("Searching a string : " + str); //Для цього уроку, те що потрібно зробити це зайве :-)
+
+            // Задача в тому, що треба просто зробити класс, через котрий будет виконуватися робота, але цей клас передае через себе інформацію в інший клас. Що і відображено у 12 і 13 строці коду.
 
             Book book = new Book();
             book.FindNext(str);
